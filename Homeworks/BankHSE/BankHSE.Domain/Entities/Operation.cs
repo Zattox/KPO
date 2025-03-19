124483631f64ca49014d116e200f55387e309ce3
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-03-20 01:44:45 +0300

    Add Operation.cs

---
+++ Homeworks/BankHSE/BankHSE.Domain/Entities/Operation.cs
@@ -0,0 +1,37 @@
+﻿using BankHSE.Domain.Enums;
+using BankHSE.Domain.Abstractions;
+
+namespace BankHSE.Domain.Entities;
+
+public class Operation : IIdentifiable, ICoreEntityVisitable
+{
+    public Guid Id { get; }
+    public TransactionType Type { get; }
+    public Guid BankAccountId { get; private set; }
+    public decimal Amount { get; private set; }
+    public DateTime Date { get; private set; }
+    public string Description { get; private set; }
+    public Guid CategoryId { get; private set; }
+
+    public Operation(TransactionType type, Guid bankAccountId, decimal amount, DateTime date, string description,
+        Guid categoryId)
+    {
+        if (amount <= 0)
+        {
+            throw new ArgumentException("Amount must be positive.");
+        }
+
+        Id = Guid.NewGuid();
+        Type = type;
+        BankAccountId = bankAccountId;
+        Amount = amount;
+        Date = date;
+        Description = description;
+        CategoryId = categoryId;
+    }
+
+    public void Accept(ICoreEntityVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
