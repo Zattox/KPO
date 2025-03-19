75302e3642b0c3951c3f71dd002ae09415ef4440
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-03-20 01:44:23 +0300

    Add Category.cs

---
+++ Homeworks/BankHSE/BankHSE.Domain/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using BankHSE.Domain.Enums;
+using BankHSE.Domain.Abstractions;
+
+namespace BankHSE.Domain.Entities;
+
+public class Category : IIdentifiable, ICoreEntityVisitable
+{
+    public Guid Id { get;}
+    public TransactionType Type { get; private set; }
+    public string Name { get; private set; }
+
+    private Category(TransactionType type, string name)
+    {
+        Id = Guid.NewGuid();
+        Type = type;
+        Name = name;
+    }
+    
+    public void Accept(ICoreEntityVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
