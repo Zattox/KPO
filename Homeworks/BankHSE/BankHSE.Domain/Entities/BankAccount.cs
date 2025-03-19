0fdd219ba65435a97f80820a5d4b313293f77c71
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-03-20 01:43:42 +0300

    Add BankAccount.cs

---
+++ Homeworks/BankHSE/BankHSE.Domain/Entities/BankAccount.cs
@@ -0,0 +1,32 @@
+﻿using BankHSE.Domain.Abstractions;
+
+namespace BankHSE.Domain.Entities;
+
+public class BankAccount : IIdentifiable, ICoreEntityVisitable
+{
+    public Guid Id { get; }
+    public string Name { get; private set; }
+    public decimal Balance { get; private set; }
+
+    public BankAccount(string name, decimal balance)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Balance = balance;
+    }
+
+    public void IncreaseBalance(decimal amount)
+    {
+        Balance += amount;
+    }
+
+    public void DecreaseBalance(decimal amount)
+    {
+        Balance -= amount;
+    }
+
+    public void Accept(ICoreEntityVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
