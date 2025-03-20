87bfc422db43729c0fab08d2a8e144224a7cab26
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-03-20 19:40:27 +0300

    Add client actions

---
+++ Homeworks/BankHSE/BankHSE.Application/Repositories/InMemoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using BankHSE.Domain.Abstractions;
+
+namespace BankHSE.Application.Repositories
+{
+    public class InMemoryRepository<T> : IRepository<T> where T : IIdentifiable
+    {
+        private readonly List<T> _entities = new();
+
+        public void Add(T entity) => _entities.Add(entity);
+        public T GetById(Guid id) => _entities.FirstOrDefault(e => e.Id == id);
+        public IEnumerable<T> GetAll() => _entities.AsReadOnly();
+
+        public void Update(T entity)
+        {
+            var index = _entities.FindIndex(e => e.Id == entity.Id);
+            if (index != -1) _entities[index] = entity;
+        }
+
+        public void Delete(Guid id) => _entities.RemoveAll(e => e.Id == id);
+    }
+}
--- Homeworks/BankHSE/BankHSE/Program.cs
+++ Homeworks/BankHSE/BankHSE/Program.cs
@@ -1,3 +1,77 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using BankHSE.Application.Analytics;
+using BankHSE.Application.Commands;
+using BankHSE.Application.Export;
+using BankHSE.Application.Facades;
+using BankHSE.Application.Factories;
+using BankHSE.Application.Import;
+using BankHSE.Application.Repositories;
+using BankHSE.Domain.Abstractions;
+using BankHSE.Domain.Entities;
+using BankHSE.Domain.Enums;
+using Microsoft.Extensions.DependencyInjection;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        var services = new ServiceCollection()
+            .AddSingleton<IRepository<BankAccount>, InMemoryRepository<BankAccount>>()
+            .AddSingleton<IRepository<Category>, InMemoryRepository<Category>>()
+            .AddSingleton<IRepository<Operation>, InMemoryRepository<Operation>>()
+            .AddSingleton<CoreEntitiesFactory>()
+            .AddSingleton<BankAccountFacade>()
+            .AddSingleton<CategoryFacade>()
+            .AddSingleton<OperationFacade>()
+            .AddSingleton<AnalyticsService>()
+            .AddSingleton<ICoreEntitiesAggregator, CoreEntitiesAggregator>()
+            .AddSingleton<JsonExporter>()
+            .AddSingleton<JsonImporter>()
+            .BuildServiceProvider();
+
+        var bankFacade = services.GetRequiredService<BankAccountFacade>();
+        var catFacade = services.GetRequiredService<CategoryFacade>();
+        var opFacade = services.GetRequiredService<OperationFacade>();
+        var analytics = services.GetRequiredService<AnalyticsService>();
+        var exporter = services.GetRequiredService<JsonExporter>();
+        var importer = services.GetRequiredService<JsonImporter>();
+
+        var account = bankFacade.CreateAccount("Основной счет", 0m);
+        var incomeCat = catFacade.CreateCategory(TransactionType.Income, "Зарплата");
+        var expenseCat = catFacade.CreateCategory(TransactionType.Expense, "Кафе");
+
+        var opCommand = new CreateOperationCommand(opFacade, TransactionType.Income, account.Id, 1000m, DateTime.Now,
+            "Зарплата за март", incomeCat.Id);
+        var timedCommand = new TimingCommand(opCommand);
+        timedCommand.Execute();
+
+        opCommand = new CreateOperationCommand(opFacade, TransactionType.Expense, account.Id, 300m, DateTime.Now,
+            "Обед в кафе", expenseCat.Id);
+        timedCommand = new TimingCommand(opCommand);
+        timedCommand.Execute();
+
+        Console.WriteLine($"Баланс счета: {account.Balance}");
+
+        var difference = analytics.GetDifferenceByAccountId(account.Id, DateTime.Now.AddDays(-1), DateTime.Now);
+        Console.WriteLine($"Разница доходов и расходов: {difference}");
+
+        var grouped = analytics.GroupOperationsByCategory(account.Id, DateTime.Now.AddDays(-1), DateTime.Now);
+        foreach (var kvp in grouped)
+            Console.WriteLine($"Категория {catFacade.GetCategoryById(kvp.Key).Name}: {kvp.Value}");
+
+        // Путь в корень проекта
+        string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", ".."));
+        string exportPath = Path.Combine(Path.Combine(projectRoot, "Data"), "data.json");
+        exporter.Export(exportPath);
+        Console.WriteLine($"Данные экспортированы в {exportPath}");
+
+        foreach (var op in opFacade.GetAll().ToList()) opFacade.Delete(op.Id);
+        foreach (var cat in catFacade.GetAllCategories().ToList()) catFacade.DeleteCategoryById(cat.Id);
+        foreach (var acc in bankFacade.GetAllAccounts().ToList()) bankFacade.DeleteAccountById(acc.Id);
+
+        importer.ImportAll(exportPath);
+        Console.WriteLine($"Импортировано счетов: {bankFacade.GetAllAccounts().Count()}");
+        Console.WriteLine($"Импортировано категорий: {catFacade.GetAllCategories().Count()}");
+        Console.WriteLine($"Импортировано операций: {opFacade.GetAll().Count()}");
+        Console.WriteLine($"Баланс после импорта: {bankFacade.GetAccountById(account.Id).Balance}");
+    }
+}
