8ddaf6a86ed7b93ab5f1f0c13e1ff368d6716862
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-03-20 19:38:28 +0300

    Refactor code

--- Homeworks/BankHSE/BankHSE.Application/Factories/CoreEntitiesFactory.cs
+++ Homeworks/BankHSE/BankHSE.Application/Factories/CoreEntitiesFactory.cs
@@ -5,19 +5,16 @@ namespace BankHSE.Application.Factories;
 
 public class CoreEntitiesFactory
 {
-    public BankAccount CreateBankAccount(string name, decimal balance = decimal.Zero)
-    {
-        return new BankAccount(name, balance);
-    }
+    public BankAccount CreateBankAccount(string name, decimal balance = decimal.Zero) => new(name, balance);
+    public BankAccount CreateBankAccount(Guid id, string name, decimal balance) => new(id, name, balance);
 
-    public Category CreateCategory(TransactionType type, string name)
-    {
-        return new Category(type, name);
-    }
+    public Category CreateCategory(TransactionType type, string name) => new(type, name);
+    public Category CreateCategory(Guid id, TransactionType type, string name) => new(id, type, name);
 
     public Operation CreateOperation(TransactionType type, BankAccount bankAccount, decimal amount, DateTime date,
-        string description, Category category)
-    {
-        return new Operation(type, bankAccount.Id, amount, date, description, category.Id);
-    }
+        string description, Category category) => new(type, bankAccount.Id, amount, date, description, category.Id);
+
+    public Operation CreateOperation(Guid id, TransactionType type, BankAccount bankAccount, decimal amount,
+        DateTime date,
+        string description, Category category) => new(id, type, bankAccount.Id, amount, date, description, category.Id);
 }
