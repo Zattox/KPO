3b468913703f88dc78cf5900c962254c153514b8
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-04-18 23:16:52 +0300

    Add StatisticsController

---
+++ Homeworks/ZooManagement/ZooManagement.Application/DTOs/ZooStatisticsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ZooManagement.Application.DTOs
+{
+    // Data transfer object for zoo statistics
+    public class ZooStatisticsDto
+    {
+        public int TotalAnimals { get; set; }
+        public int FreeEnclosures { get; set; }
+    }
+}
---
+++ Homeworks/ZooManagement/ZooManagement.Presentation/Controllers/StatisticsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ZooManagement.Application.Services;
+using ZooManagement.Application.DTOs;
+
+namespace ZooManagement.Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/statistics")]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly ZooStatisticsService _zooStatisticsService;
+
+        public StatisticsController(ZooStatisticsService zooStatisticsService)
+        {
+            _zooStatisticsService = zooStatisticsService ?? throw new ArgumentNullException(nameof(zooStatisticsService));
+        }
+
+        // Retrieves zoo statistics
+        [HttpGet]
+        public ActionResult<ZooStatisticsDto> GetStatistics()
+        {
+            var stats = new ZooStatisticsDto
+            {
+                TotalAnimals = _zooStatisticsService.GetTotalAnimals(),
+                FreeEnclosures = _zooStatisticsService.GetFreeEnclosures()
+            };
+            return Ok(stats);
+        }
+    }
+}
