59661cfadfded706eaf689bbcf5aa54205dca38c
Zattox <vladcsgoogurechik@mail.ru>
2025-04-18 18:35:18 +0300

    Add templates for Mini HW-2

---
+++ Homeworks/ZooManagement/ZooManagement.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZooManagement.Domain
+{
+    public class Class1
+    {
+    }
+}
---
+++ Homeworks/ZooManagement/ZooManagement.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ZooManagement.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
