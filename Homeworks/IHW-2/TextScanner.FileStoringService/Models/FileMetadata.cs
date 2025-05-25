d3578407b5e61447482f6d9a0bffbf413c7f66b4
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-05-25 13:49:10 +0300

    Add FileMetadata

---
+++ Homeworks/IHW-2/TextScanner.FileStoringService/Models/FileMetadata.cs
@@ -0,0 +1,8 @@
+﻿namespace TextScanner.FileStoringService.Models;
+
+public class FileMetadata
+{
+    public string Id { get; set; }
+    public string FileName { get; set; }
+    public string StoragePath { get; set; }
+}
