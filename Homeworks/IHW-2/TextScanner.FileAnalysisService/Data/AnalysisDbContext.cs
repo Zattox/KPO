b5afeaf5f4a569f56b154bd291969c3de9272292
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-05-25 13:50:07 +0300

    Add AnalysisDbContext

---
+++ Homeworks/IHW-2/TextScanner.FileAnalysisService/Data/AnalysisDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TextScanner.FileAnalysisService.Models;
+
+namespace TextScanner.FileAnalysisService.Data
+{
+    public class AnalysisDbContext : DbContext
+    {
+        public DbSet<AnalysisResult> AnalysisResults { get; set; }
+
+        public AnalysisDbContext(DbContextOptions<AnalysisDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AnalysisResult>()
+                .Property(a => a.Id)
+                .ValueGeneratedOnAdd(); // Указываем, что Id автоинкрементируется
+        }
+    }
+}
