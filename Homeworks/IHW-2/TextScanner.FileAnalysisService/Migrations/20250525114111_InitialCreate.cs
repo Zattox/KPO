e7656c56dc979f1f625eb94cd0ee6a6818c0960c
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-05-25 14:44:55 +0300

    Create migrations

---
+++ Homeworks/IHW-2/TextScanner.FileAnalysisService/Migrations/20250525114111_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TextScanner.FileAnalysisService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AnalysisResults",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FileId = table.Column<string>(type: "text", nullable: false),
+                    ParagraphCount = table.Column<int>(type: "integer", nullable: false),
+                    WordCount = table.Column<int>(type: "integer", nullable: false),
+                    CharacterCount = table.Column<int>(type: "integer", nullable: false),
+                    IsPlagiarized = table.Column<bool>(type: "boolean", nullable: false),
+                    Hash = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AnalysisResults", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AnalysisResults");
+        }
+    }
+}
---
+++ Homeworks/IHW-2/TextScanner.FileStoringService/Migrations/20250525114019_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TextScanner.FileStoringService.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileMetadatas",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    StoragePath = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileMetadatas", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileMetadatas");
+        }
+    }
+}
