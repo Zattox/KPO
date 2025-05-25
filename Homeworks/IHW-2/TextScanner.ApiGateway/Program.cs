73c37b9e869e144fbb2615e85648197269c1cbc3
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-05-25 13:51:24 +0300

    Add Service settings

--- Homeworks/IHW-2/TextScanner.ApiGateway/Program.cs
+++ Homeworks/IHW-2/TextScanner.ApiGateway/Program.cs
@@ -1,44 +1,42 @@
+using DotNetEnv;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "API Gateway", Version = "v1" });
+});
+
+var fileStoringServiceUrl = Environment.GetEnvironmentVariable("SERVICES_FILESTORINGSERVICE");
+if (string.IsNullOrEmpty(fileStoringServiceUrl))
+{
+    throw new InvalidOperationException("FileStoringService URL must be specified in the environment variables.");
+}
+
+var fileAnalysisServiceUrl = Environment.GetEnvironmentVariable("SERVICES_FILEANALYSISSERVICE");
+if (string.IsNullOrEmpty(fileAnalysisServiceUrl))
+{
+    throw new InvalidOperationException("FileAnalysisService URL must be specified in the environment variables.");
+}
+
+builder.Services.AddHttpClient("FileStoringService", client =>
+{
+    client.BaseAddress = new Uri(fileStoringServiceUrl!); // Указываем, что значение не null
+});
+
+builder.Services.AddHttpClient("FileAnalysisService", client =>
+{
+    client.BaseAddress = new Uri(fileAnalysisServiceUrl!); // Указываем, что значение не null
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Gateway v1"));
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- Homeworks/IHW-2/TextScanner.FileStoringService/Program.cs
+++ Homeworks/IHW-2/TextScanner.FileStoringService/Program.cs
@@ -1,44 +1,26 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using TextScanner.FileStoringService.Data;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<FileStorageDbContext>(options =>
+    options.UseNpgsql($"Host={Environment.GetEnvironmentVariable("DB_HOST")};" +
+                      $"Port={Environment.GetEnvironmentVariable("DB_PORT")};" +
+                      $"Database={Environment.GetEnvironmentVariable("DB_DATABASE")};" +
+                      $"Username={Environment.GetEnvironmentVariable("DB_USERNAME")};" +
+                      $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD")}"));
+
+builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new() { Title = "File Storing Service", Version = "v1" }); });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "File Storing Service v1"));
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
